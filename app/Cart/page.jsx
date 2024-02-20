6b1e229efaeb27d21e8b2d12fc8240014f8499b7
arcosane <castelinoaston123@gmail.com>
2024-02-20 16:27:50 +0530

    firebase

--- app/Cart/page.jsx
+++ app/Cart/page.jsx
@@ -1,13 +1,42 @@
+"use client"
+import {DataSnapshot, get, ref} from 'firebase/database';
 import Navbar from '@/components/Navbar'
 import React from 'react'
+import { useEffect,useState } from 'react';
+import {database} from '@/lib/firebase';
 
 export default function page() {
+  const [items,setItems] = useState([]);
+  useEffect(() => {
+    const usersRef = ref(database,'items');
+    get(usersRef).then((DataSnapshot) => {
+      if(DataSnapshot.exists()) {
+        const userArray = Object.entries(DataSnapshot.val()).map(([id, data]) => ({
+          id,
+          ...data,
+        }));
+        setItems(userArray);
+      }
+      else{
+        console.log('No data available')
+      }
+
+    }).catch((error) => {
+      console.error(error);
+    });
+  }, []);
   return (
     <>   
         <div className="sticky top-0">
         <Navbar/>
       </div>
-        <div>Cart</div>
+        <div>
+          {items.map((items) => (
+             <div key={items.id}>
+              <h2>{items.id}</h2>
+             </div>
+          ))}
+        </div>
     </>
   )
 }
