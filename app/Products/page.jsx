e9e2d749aea4b1ea93e5361b16a256c5c9213d58
Chhand Chaughule <chhandchaughule123@gmail.com>
2024-02-20 15:13:17 +0530

    products

--- app/Products/page.jsx
+++ app/Products/page.jsx
@@ -1,9 +1,36 @@
+"use client"
+
 import Image from 'next/image';
 import Item_card from '@/components/item_card';
-import React from 'react'
+import React , { useState,useEffect } from 'react'
 import Navbar from '@/components/Navbar';
 
 export default function page() {
+   
+  const Items = [
+    {
+        "picUrl":'/assets/img/hero.jpg',
+        "name":"mango",
+        "price":120,
+    },
+    {
+        "picUrl":'/assets/img/hero.jpg',
+        "name":"apple",
+        "price":120
+    },
+    {
+        "picUrl":'/assets/img/hero.jpg',
+        "name":"banana",
+        "price":120
+    },
+    {
+        "picUrl":'/assets/img/hero.jpg',
+        "name":"strawberry",
+        "price":120
+    },
+  ]
+    
+
   return (
     <>
     <div className="sticky top-0">
@@ -13,14 +40,10 @@ export default function page() {
         <div className='p-5 bg-slate-200'>
             <h1 className='pl-5 text-4xl font-extrabold text-orange-500'>Popular Products</h1>
             <div className='flex flex-row p-5 w-full gap-5 overflow-x-auto'>
-                <Item_card/>
-                <Item_card/>
-                <Item_card/>
-                <Item_card/>
-                <Item_card/>
-                <Item_card/>
-                <Item_card/>
-                <Item_card/>
+                        {/* Map through the Items array and render each Item_card */}
+                        {Items.map((item, index) => (
+                            <Item_card key={index} itemPic={item.picUrl} itemName={item.name} itemPrice={item.price} />
+                        ))}
             </div>
         </div>
         <div className='p-5 bg-yellow-300'>
